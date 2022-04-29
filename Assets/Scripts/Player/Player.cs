06f4c2f33d4efc4d8e3c1593555c48f6e5284e25
alexandre.nascimento.ifms <alexandre.nascimento.ifms@gmail.com>
2022-04-28 20:52:24 -0400

    :sparkles: implementando pulo

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,12 +6,20 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody2D;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(0.1f, 0);
 
     public float speed;
 
+    public float forceJump = 25;
 
-    void Update()
+
+    private void Update()
+    {
+        HandleJump();
+        HandleMoviment();
+    }
+
+    private void HandleMoviment()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -22,6 +30,23 @@ public class Player : MonoBehaviour
         {
             // BAD CODE: myRigidbody2D.MovePosition(myRigidbody2D.position + velocity * Time.deltaTime);
             myRigidbody2D.velocity = new Vector2(speed, myRigidbody2D.velocity.y);
+        }
+
+        if (myRigidbody2D.velocity.x > 0)
+        {
+            myRigidbody2D.velocity += friction;
+        }
+        else if (myRigidbody2D.velocity.x < 0)
+        {
+            myRigidbody2D.velocity -= friction;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody2D.velocity = Vector2.up * forceJump;
         }
     }
 }
